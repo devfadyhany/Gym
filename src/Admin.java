67f3a1c9118fca3767c6d717899feea9ddebe469
Ziadtopg <ziad.saeed16@hotmail.com>
2023-11-26 16:45:09 +0200

    Membership_Plan and Admin classes created

---
+++ src/Admin.java
@@ -0,0 +1,40 @@
+public class Admin {
+    private int ID;
+    private String Name;
+    private String Email;
+    private String Password;
+    Admin(int i,String n,String e,String p){
+        this.ID=i;
+        this.Name=n;
+        this.Email=e;
+        this.Password=p;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
---
+++ src/Membership_Plan.java
@@ -0,0 +1,85 @@
+import java.util.Date;
+
+public class Membership_Plan {
+    private Date Start_Date;
+    private char Monthly_plan;
+    private int Registred_Months_num;
+    private float Plan_price;
+    Membership_Plan(Date d,char c,int i,float f){
+        this.Start_Date=d;
+        this.Monthly_plan=c;
+        this.Registred_Months_num=i;
+        this.Plan_price=f;
+
+    }
+
+    public void setStart_Date(Date start_Date) {
+        Start_Date = start_Date;
+    }
+
+    public Date getStart_Date() {
+        return Start_Date;
+    }
+
+    public void setMonthly_plan(char monthly_plan) {
+        if(monthly_plan=='a' || monthly_plan =='b'|| monthly_plan=='A'||monthly_plan=='B')
+            Monthly_plan = monthly_plan;
+        else
+            System.out.println("plan is not recognizable, choose between plan A or Plan b");
+    }
+
+    public char getMonthly_plan() {
+        return Monthly_plan;
+    }
+
+    public void setRegistred_Months_num(int registred_Months_num) {
+        Registred_Months_num = registred_Months_num;
+    }
+
+    public int getRegistred_Months_num() {
+        return Registred_Months_num;
+    }
+
+    public void setPlan_price(float plan_price) {
+        Plan_price = plan_price;
+    }
+
+    public float getPlan_price() {
+        return Plan_price;
+    }
+    public float CalcDiscount() {
+        if (this.Monthly_plan == 'a' || this.Monthly_plan == 'A') {
+            if (this.Registred_Months_num >= 3) {
+                switch (Registred_Months_num) {
+                    case 3:
+                        this.Plan_price -= this.Plan_price *= 0.2F;
+                    case 6:
+                        this.Plan_price -= this.Plan_price *= 0.3F;
+                    case 12:
+                        this.Plan_price -= this.Plan_price *= 0.4F;
+                    default:
+                        System.out.println("No discount will be applied ");
+                }
+
+
+            }
+        } else if (this.Monthly_plan=='b'|| this.Monthly_plan=='B')
+        {
+            switch (Registred_Months_num) {
+                case 3:
+                    this.Plan_price -= this.Plan_price *= 0.3F;
+                case 6:
+                    this.Plan_price -= this.Plan_price *= 0.4F;
+                case 12:
+                    this.Plan_price -= this.Plan_price *= 0.5F;
+                default:
+                    this.Plan_price+=this.Plan_price*=0.15F;
+            }
+
+
+        }
+        return this.Plan_price;
+    }
+
+}
+
