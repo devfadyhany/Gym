9f3deb4bb09e82616e24d76c1c9faf1e4af0e154
amaamoun99 <abdelrahman.maamoun@codeology.digital>
2023-12-01 21:00:54 +0200

    Login/Register

--- src/Gym.java
+++ src/Gym.java
@@ -7,11 +7,10 @@ public class Gym {
     public static Customer[] Customers;
     public static Admin[] Admins;
     private Equipment[] Sports_equipment;
-    private int numberOfEquipments= 0;
+    private int numberOfEquipments = 0;
     public static Coach[] Coaches;
     private Subscription[] Subscriptions;
     private int numberOfSubscriptions = 0;
-
 
 
     public Gym(String Name, String Address, String Phone_number, Equipment[] Sports_equipment, Subscription[] Subscriptions) {
@@ -26,6 +25,7 @@ public class Gym {
     public void setName(String Name) {
         this.Name = Name;
     }
+
     public String getName() {
         return Name;
     }
@@ -33,6 +33,7 @@ public class Gym {
     public void setAddress(String Address) {
         this.Address = Address;
     }
+
     public String getAddress() {
         return Address;
     }
@@ -40,6 +41,7 @@ public class Gym {
     public void setPhoneNumber(String Phone_number) {
         this.Phone_number = Phone_number;
     }
+
     public String getPhoneNumber() {
         return Phone_number;
     }
@@ -48,6 +50,7 @@ public class Gym {
 
         this.numberOfEquipments = numberOfEquipments;
     }
+
     public int getNumberOfEquipments() {
 
         return numberOfEquipments;
@@ -57,19 +60,21 @@ public class Gym {
 
         this.numberOfSubscriptions = numberOfSubscriptions;
     }
+
     public int getNumberOfSubscriptions() {
 
         return numberOfSubscriptions;
     }
 
-    public void AddEquipment(Equipment equipment){
+    public void AddEquipment(Equipment equipment) {
 
         Sports_equipment[numberOfEquipments] = equipment;
     }
-    public void RemoveEquipment(int Code){
+
+    public void RemoveEquipment(int Code) {
         int removedIndex = 0;
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             if (Sports_equipment[i].getEQUIPMENTCODE() == Code) {
                 Sports_equipment[i] = null;
                 removedIndex = i;
@@ -77,15 +82,16 @@ public class Gym {
             }
         }
 
-        for (int j = 0; j < 10; j++){
-            if (j >= removedIndex){
-                Sports_equipment[j] = Sports_equipment[j+1];
+        for (int j = 0; j < 10; j++) {
+            if (j >= removedIndex) {
+                Sports_equipment[j] = Sports_equipment[j + 1];
             }
         }
     }
-    public void ViewEquipments(int ID){
-        for (int i = 0; i < 10; i++){
-            if (Sports_equipment[i].getEQUIPMENTCODE() == ID){
+
+    public void ViewEquipments(int ID) {
+        for (int i = 0; i < 10; i++) {
+            if (Sports_equipment[i].getEQUIPMENTCODE() == ID) {
                 Sports_equipment[i].DisplayInfo();
                 System.out.println("=================");
                 break;
@@ -93,13 +99,14 @@ public class Gym {
         }
     }
 
-    public static void AddAdmin(Admin admin){
+    public static void AddAdmin(Admin admin) {
         Admins[Admin.adminsCount] = admin;
     }
-    public static void RemoveAdmin(int ID){
+
+    public static void RemoveAdmin(int ID) {
         int removedIndex = 0;
 
-        for(int i = 0; i < Admin.adminsCount; i++) {
+        for (int i = 0; i < Admin.adminsCount; i++) {
             if (Admins[i].getID() == ID) {
                 Admins[i] = null;
                 removedIndex = i;
@@ -107,18 +114,19 @@ public class Gym {
             }
         }
 
-        for (int j = 0; j < Admin.adminsCount; j++){
+        for (int j = 0; j < Admin.adminsCount; j++) {
             if (j < Admin.adminsCount - 1) {
-                if (j >= removedIndex){
-                    Admins[j] = Admins[j+1];
+                if (j >= removedIndex) {
+                    Admins[j] = Admins[j + 1];
                 }
             }
         }
         Admin.adminsCount--;
     }
-    public static void ViewAdmins(int ID){
-        for (int i = 0; i < Admin.adminsCount; i++){
-            if (Admins[i].getID() == ID){
+
+    public static void ViewAdmins(int ID) {
+        for (int i = 0; i < Admin.adminsCount; i++) {
+            if (Admins[i].getID() == ID) {
                 Admins[i].DisplayInfo();
                 System.out.println("=================");
                 break;
@@ -126,13 +134,14 @@ public class Gym {
         }
     }
 
-    public static void AddCustomer(Customer customer){
+    public static void AddCustomer(Customer customer) {
         Customers[Customer.customersCount] = customer;
     }
-    public static void RemoveCustomer(int ID){
+
+    public static void RemoveCustomer(int ID) {
         int removedIndex = 0;
 
-        for(int i = 0; i < Customer.customersCount; i++) {
+        for (int i = 0; i < Customer.customersCount; i++) {
             if (Customers[i].getID() == ID) {
                 Customers[i] = null;
                 removedIndex = i;
@@ -140,18 +149,19 @@ public class Gym {
             }
         }
 
-        for (int j = 0; j < Customer.customersCount; j++){
+        for (int j = 0; j < Customer.customersCount; j++) {
             if (j < Customer.customersCount - 1) {
-                if (j >= removedIndex){
-                    Customers[j] = Customers[j+1];
+                if (j >= removedIndex) {
+                    Customers[j] = Customers[j + 1];
                 }
             }
         }
         Customer.customersCount--;
     }
-    public static void ViewCustomers(int ID){
-        for (int i = 0; i < Customer.customersCount; i++){
-            if (Customers[i].getID() == ID){
+
+    public static void ViewCustomers(int ID) {
+        for (int i = 0; i < Customer.customersCount; i++) {
+            if (Customers[i].getID() == ID) {
                 Customers[i].DisplayInfo();
                 System.out.println("=================");
                 break;
@@ -159,13 +169,14 @@ public class Gym {
         }
     }
 
-    public static void AddCoach(Coach coach){
+    public static void AddCoach(Coach coach) {
         Coaches[Coach.coachCount] = coach;
     }
-    public static void RemoveCoach(int ID){
+
+    public static void RemoveCoach(int ID) {
         int removedIndex = 0;
 
-        for(int i = 0; i < Coach.coachCount; i++) {
+        for (int i = 0; i < Coach.coachCount; i++) {
             if (Coaches[i].getID() == ID) {
                 Coaches[i] = null;
                 removedIndex = i;
@@ -173,18 +184,19 @@ public class Gym {
             }
         }
 
-        for (int j = 0; j < Coach.coachCount; j++){
+        for (int j = 0; j < Coach.coachCount; j++) {
             if (j < Coach.coachCount - 1) {
-                if (j >= removedIndex){
-                    Coaches[j] = Coaches[j+1];
+                if (j >= removedIndex) {
+                    Coaches[j] = Coaches[j + 1];
                 }
             }
         }
         Coach.coachCount--;
     }
-    public static void ViewCoaches(int ID){
-        for (int i = 0; i < Coach.coachCount; i++){
-            if (Coaches[i].getID() == ID){
+
+    public static void ViewCoaches(int ID) {
+        for (int i = 0; i < Coach.coachCount; i++) {
+            if (Coaches[i].getID() == ID) {
                 Coaches[i].DisplayInfo();
                 System.out.println("=================");
                 break;
@@ -192,14 +204,15 @@ public class Gym {
         }
     }
 
-    public void AddSubscription(Subscription Subscription){
+    public void AddSubscription(Subscription Subscription) {
 
         Subscriptions[numberOfSubscriptions] = Subscription;
     }
-    public void RemoveSubscriptions(int ID){
+
+    public void RemoveSubscriptions(int ID) {
         int removedIndex = 0;
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             if (Subscriptions[i].getID() == ID) {
                 Subscriptions[i] = null;
                 removedIndex = i;
@@ -207,19 +220,32 @@ public class Gym {
             }
         }
 
-        for (int j = 0; j < 10; j++){
-            if (j >= removedIndex){
-                Subscriptions[j] = Subscriptions[j+1];
+        for (int j = 0; j < 10; j++) {
+            if (j >= removedIndex) {
+                Subscriptions[j] = Subscriptions[j + 1];
             }
         }
     }
-    public void ViewSubscriptions(int ID){
-        for (int i = 0; i < 10; i++){
-            if (Subscriptions[i].getID() == ID){
+
+    public void ViewSubscriptions(int ID) {
+        for (int i = 0; i < 10; i++) {
+            if (Subscriptions[i].getID() == ID) {
                 Subscriptions[i].DisplaySubscriptionInfo();
                 System.out.println("=================");
                 break;
             }
         }
     }
+
+    public static Coach SearchCoachByID(int id) {
+        int index=0;
+        for (int i = 0; i < Coach.coachCount; i++) {
+            if (Coaches[i].getID() == id) {
+                index=i;
+                break;
+            }
+        }
+        return Coaches[index];
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,88 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        boolean flag;
+        do {
+            int choice;
+             flag=false;
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is your role 1-coash    2-client    3-admin)");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    LoginOrRegister(choice);
+                    flag=true;
+                   break;
+                case 2:
+                    LoginOrRegister(choice);
+                    flag=true;
+                    break;
+                case 3:
+                    LoginMenu(choice);
+                    flag=true;
+                    break;
+            }
+        } while (flag = false);
+
+    }
+
+    public static  void LoginOrRegister(int choice) {
+        int newchoice;
+        do {
+            newchoice = 0;
+            Scanner input = new Scanner(System.in);
+            System.out.println("1- Login      2-Register");
+            newchoice = input.nextInt();
+            switch (newchoice) {
+                case 1:
+                    LoginMenu(choice);
+                    newchoice=0;
+                    break;
+                case 2:
+                    RegisterMenu(choice);
+                    newchoice=0;
+                    break;
+            }
+
+        }  while (newchoice != 0) ;
+    }
+
+    public static void LoginMenu(int choice){
+        String email,password;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter your email");
+        email=input.next();
+        System.out.println("enter your password");
+        password=input.next();
+
+        if (choice==1){
+            Login.CoachLogin(email,password);
+        } else if (choice==2) {
+            Login.CustomerLogin(email,password);
+        }
+       else {
+          Login.AdminLogin(email,password);
+        }
+    }
+
+    public static void RegisterMenu(int choice){
+        Scanner input=new Scanner(System.in);
+        String name,email,password,phoneNumber;
+        char gender;
+        System.out.println("enter your name");
+        name=input.next();
+        System.out.println("enter your email");
+        email=input.next();
+        System.out.println("enter your password");
+        password=input.next();
+        System.out.println("enter your phone number");
+        phoneNumber=input.next();
+        System.out.println("enter your gender");
+        gender=input.next().charAt(0);
     }
 }
+
